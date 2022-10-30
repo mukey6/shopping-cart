fa72b3d4c4c19081fb4589db3a192ba029ab5dd7
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-10-30 10:48:51 -0500

    adding store items and images

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,15 @@
+import { Col, Row } from 'react-bootstrap'
+import { json } from 'react-router-dom'
+import { StoreItems } from '../component/StoreItems'
+import storeItems from '../data/items.json'
+
 export function Store(){
-    return <h1>sore</h1>
+    return <>
+    <h1>store</h1>
+    <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map(item => (
+        <Col key={item.id}><StoreItems {...item}/></Col>))}
+    </Row>
+    </> 
+    
 }
