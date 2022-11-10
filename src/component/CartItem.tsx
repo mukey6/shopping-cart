d1afa4e92527921d898fbfcf53b785188ed7a59c
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-11-09 18:00:26 -0600

    display items in cart to show amount

--- src/component/CartItem.tsx
+++ src/component/CartItem.tsx
@@ -1,20 +1,39 @@
-import { Stack } from "react-bootstrap"
-import { useShoppingCart } from "../context/ShoppingCartContext"
-import storeItems from '../data/items.json'
-type CartItemProps ={
-    id: number
-    quantity: number
-}
-export function CartItem({id, quantity}:CartItemProps){
-    const {removeFromCart} = useShoppingCart()
-    const item = storeItems.find(i => i.id === id)
-    if(item === null){
-        return null
-    }
-    return (
-        <Stack direction="horizontal" gap={2}>
-            <img src={item.imgUrl} style={{width:'125px', height:'75px', objectFit:'cover'}} />
+import { Button, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import storeItems from "../data/items.json";
+import { FormatCurrency } from "../utils/formatCurrency";
+type CartItemProps = {
+  id: number;
+  quantity: number;
+};
+export function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart();
+  const item = storeItems.find((i) => i.id === id);
+  if (item == null) return null;
 
-        </Stack>
-    )
+  return (
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {FormatCurrency(item.price)}
+        </div>
+        <div>
+        {FormatCurrency(item.price * quantity)}
+        </div>
+        <Button variant="outline-danger" size="sm" onClick={()=> removeFromCart(item.id)}>&times;</Button>
+      </div>
+    </Stack>
+  );
 }
